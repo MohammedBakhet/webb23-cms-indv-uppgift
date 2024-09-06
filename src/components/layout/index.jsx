b9ec85d9da642541720a6b92a9a1b38fa4f749df
Mohammed Bakhet <momo.baket@gmail.com>
2024-09-06 21:29:11 +0200

    last

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,28 +1,27 @@
 import Image from "next/image";
 import Header from "../nestable/Header";
 
-
 export default function Layout({ config, children }) {
 
-  const logotype = config.content.logotype.filename;
+  const logotype = config?.content?.logotype?.filename || "/default-logo.png";
+  const content = config?.content || {}; // Fallback if content is undefined
 
   return (
     <>
       <header className="bg-red-800 text-white p-4 shadow-xl">
         <div className="container mx-auto flex items-center justify-between">
-         
-        <Image 
-  src={logotype} 
-  width={150} 
-  height={150}  // Ensure width and height are the same for a round image
-  className="rounded-full object-cover" // Tailwind class to make it round
-/>
+          <Image 
+            src={logotype} 
+            width={150} 
+            height={150} 
+            className="rounded-full object-cover" 
+            alt="Logo"
+          />
           
-          <Header blok={config.content} /> 
+          <Header blok={content} /> 
         </div>
       </header>
-      <main>
-      {children}</main>
+      <main>{children}</main>
       <footer className="bg-red-800 text-white p-4">
         <p>Footer</p>
       </footer>
