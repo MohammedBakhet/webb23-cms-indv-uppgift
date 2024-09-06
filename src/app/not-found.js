9450586cb1f677c5e538b51ea1b2bdfee32b9e43
Mohammed Bakhet <momo.baket@gmail.com>
2024-09-06 20:38:24 +0200

    apo

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -5,7 +5,7 @@ export default function NotFound() {
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-center">
       <h1 className="text-5xl font-bold text-red-600">404</h1>
       <h2 className="text-2xl font-semibold mt-4 text-gray-800">Oops! Page not found</h2>
-      <p className="text-gray-600 mt-2">The page you're looking for doesnt exist.</p>
+      <p className="text-gray-600 mt-2">The page youre looking for doesnt exist.</p>
       
     
     </div>
